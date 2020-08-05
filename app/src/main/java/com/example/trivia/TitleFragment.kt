216637d4c0c178f1463851f470247d0789389aa7
fcpbarros <69065679+fcpbarros@users.noreply.github.com>
2020-08-04 21:11:54 -0300

    First commit on the TitleScreen branch in order to set up the fragment on the screen.

---
+++ app/src/main/java/com/example/trivia/TitleFragment.kt
@@ -0,0 +1,36 @@
+package com.example.trivia
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.trivia.databinding.TitleFragmentBinding
+
+
+class TitleFragment : Fragment() {
+
+    /**
+     * Instanciate the binding object
+     */
+    private lateinit var binding: TitleFragmentBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        /**
+         * inflate the fragment
+         */
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.title_fragment
+            , container, false
+        )
+
+
+        return binding.root
+    }
+}
