8199e35cc201f897211136a8435a059d1b205d55
fcpbarros <69065679+fcpbarros@users.noreply.github.com>
2020-08-04 22:23:39 -0300

    Fourth commit
    DrawerLayout is implemented in this update.

--- app/src/main/java/com/example/trivia/MainActivity.kt
+++ app/src/main/java/com/example/trivia/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.trivia
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.example.trivia.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        /**
+         * Code that allows the user to display the navigation drawer
+         */
+        drawerLayout = binding.drawerLayout
+        //The following line is important to define the NavController
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        //link the action bar with navController and drawer layout
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+    }
+
 }
