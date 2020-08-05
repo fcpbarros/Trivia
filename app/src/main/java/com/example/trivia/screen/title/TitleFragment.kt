5707afb4218e4a75c666d34ed7392f7dec1bf716
fcpbarros <69065679+fcpbarros@users.noreply.github.com>
2020-08-04 21:56:27 -0300

    Third commit
    Fixed a linking problem with the navigation feature by adding
    classpath "androidx.navigation:navigation-safe-args-gradle-plugin:2.2.1" to the project build gradle
    and later updating it to
    classpath "androidx.navigation:navigation-safe-args-gradle-plugin:2.3.0"

--- app/src/main/java/com/example/trivia/screen/title/TitleFragment.kt
+++ app/src/main/java/com/example/trivia/screen/title/TitleFragment.kt
@@ -1,4 +1,4 @@
-package com.example.trivia
+package com.example.trivia.screen.title
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.trivia.R
 import com.example.trivia.databinding.TitleFragmentBinding
 
 
